554261d003442a013d7f9cb588b95877f3e1c5b3
KAHFI ZAIN <zkahfi@gmail.com>
2023-11-28 09:17:37 +0700

    setup retrofit2 hit API https://newsapi.org/

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,4 +103,10 @@ dependencies {
     implementation ("androidx.paging:paging-runtime:3.2.1")
     implementation ("androidx.paging:paging-compose:3.2.0-rc01")
 
+    //Room
+    implementation ("androidx.room:room-runtime:2.6.0")
+    kapt ("androidx.room:room-compiler:2.6.0")
+    implementation ("androidx.room:room-ktx:2.6.0")
+
+
 }
--- app/src/main/java/com/kaza/newsshortsapp/domain/model/Article.kt
+++ app/src/main/java/com/kaza/newsshortsapp/domain/model/Article.kt
@@ -1,9 +1,11 @@
 package com.kaza.newsshortsapp.domain.model
 
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
+@Entity
 data class Article(
     val author: String,
     val content: String,
